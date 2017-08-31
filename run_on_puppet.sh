cb92df91c49a06539818fb6d3ac364c1d17f6ead
Boris Gonev <bgonev@gmail.com>
2017-09-01 00:04:08 +0200

    fix

--- run_on_puppet.sh
+++ run_on_puppet.sh
@@ -124,8 +124,8 @@ sudo systemctl stop firewalld
 ## Install Puppet
 sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 sudo yum -y install puppetserver
-sed -i 's/2g/512m/g' /etc/sysconfig/puppetserver /etc/sysconfig/puppetserver
-sed -i 's/-XX\:MaxPermSize\=256m//g' /etc/sysconfig/puppetserver /etc/sysconfig/puppetserver 
+sudo sed -i 's/2g/512m/g' /etc/sysconfig/puppetserver /etc/sysconfig/puppetserver
+sudo sed -i 's/-XX\:MaxPermSize\=256m//g' /etc/sysconfig/puppetserver /etc/sysconfig/puppetserver 
 
 sudo systemctl start puppetserver
 sudo systemctl enable puppetserver
