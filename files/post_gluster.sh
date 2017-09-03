64bfe82417bb67991fd6e70022c0387b8916d155
Boris Gonev <bgonev@gmail.com>
2017-09-03 16:39:42 +0200

    Fix

--- files/post_gluster.sh
+++ files/post_gluster.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo gluster peer probe nfsserver2
 sleep 10
-sudo gluster volume create br0 webshare 2 nfsserver1:/share/webshare nfsserver2:/share/webshare
+sudo gluster volume create webshare replica 2 nfsserver1:/share/webshare nfsserver2:/share/webshare
+sleep 10
 sudo gluster volume start webshare
-sudo yes | gluster volume set br0 nfs.disable off
+sleep 5
+yes | sudo gluster volume set webshare nfs.disable off
